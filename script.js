30c0ae637c91e8937655db4015cd0da02da090ec
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2016-07-28 22:54:57 -0400

    write the printEmptyCells function

--- script.js
+++ script.js
@@ -10,17 +10,18 @@ var oWinCount = 0
 var oSvg = '<svg xmlns="http://www.w3.org/2000/svg">' +
                '<circle cx="75" cy="75" r="40" fill="transparent" stroke="white" stroke-width="20"/>' +
            '</svg>';
-         
+
 var xSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="150">' +
                 '<path stroke="white" stroke-width="20"  d="M35,35 L115,115 M115,35 L35,115"/>' +
             '</svg>';
-         
+
 var clickCounter = 0;
 var gameOver = false;
 
 
 function cellClicked(div, rowIndex, colIndex){
     console.log('Button clicked: ', div, ', rowIndex: ', rowIndex, ', colIndex: ', colIndex);
+
 
     if (gameOver === false && board[rowIndex][colIndex] === " ") {
 
@@ -65,6 +66,9 @@ function cellClicked(div, rowIndex, colIndex){
 
         }
     }
+
+    printEmptyCells()
+
  }
 
 
@@ -96,7 +100,6 @@ function whoWon() {
     return winner;
   }
 
-
   return ' ';
 }
 
@@ -126,13 +129,33 @@ function whoWonColumn(){
 
 function whoWonDiagonal(){
     var whoWon = ' ';
-        if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-            whoWon = board[0][0];
-        }else if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-            whoWon = board[0][2];
-        }
+
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+        whoWon = board[0][0];
+    }else if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+        whoWon = board[0][2];
+    }
 
     return whoWon;
 }
 
+function printEmptyCells(){
+    var emptyCellCounter = 9;
 
+    for (var rowIndex=0; rowIndex<3; rowIndex++) {
+        for (var columnIndex=0; columnIndex<3; columnIndex++) {
+            if (board[rowIndex][columnIndex] === " "){
+
+                console.log('cell, rowIndex: ' + rowIndex +
+                    ', columnIndex: ' + columnIndex +
+                    ", value at board location: " + board[rowIndex][columnIndex]
+                );
+            } else {
+                emptyCellCounter -= 1;
+            }
+        }
+    }
+
+    console.log(emptyCellCounter);
+
+}
